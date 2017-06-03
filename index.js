044c65288467ec40793571286dba5fa60b26d440
avengang <544160071@qq.com>
2017-06-03 16:22:24 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by avengang on 2017/6/3 0003.
+ */
+module.exports.open = function() {
+    console.log("open the eyes");
+};
+module.exports.close = function() {
+    console.log("close the eyes");
+};
